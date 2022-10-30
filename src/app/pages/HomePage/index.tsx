c3ef285ee266c209a6dd14d191058305dfffc3ce
avyukt <aggarwal.avyukt@gmail.com>
2022-10-30 02:01:15 -0700

    avyukt's code

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,25 +1,50 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { NavBar } from 'app/components/NavBar';
-import { Masthead } from './Masthead';
-import { Features } from './Features';
-import { PageWrapper } from 'app/components/PageWrapper';
+
+import { PageHeader } from 'antd';
+import { Divider } from 'antd';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
+import { Dropdown, Menu, Space } from 'antd';
+import 'antd/dist/antd.css';
 
 export function HomePage() {
+  const menu = (
+    <Menu
+      items={[
+        {
+          key: '1',
+          label: '1st menu item',
+        },
+        {
+          key: '2',
+          label: '2nd menu item',
+        },
+        {
+          key: '3',
+          label: '3rd menu item',
+        },
+        {
+          key: '4',
+          label: '4th menu item',
+        },
+      ]}
+    />
+  );
   return (
     <>
       <Helmet>
-        <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React Boilerplate application homepage"
-        />
+        <title>HomePage</title>
+        <meta name="description" content="An Abaqus Script Creator" />
       </Helmet>
-      <NavBar />
-      <PageWrapper>
-        <Masthead />
-        <Features />
-      </PageWrapper>
+
+      <Dropdown overlay={menu}>
+        <a onClick={e => e.preventDefault()}>
+          <Space>
+            Hover me
+            <DownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
     </>
   );
 }
